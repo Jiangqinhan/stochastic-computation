4b7cfe55d639f72d86149cecb4d5d46fd35508ef
JQH <514050819@qq.com>
2020-03-11 21:44:05 +0800

    Euler scheme,Milstein scheme and strong order 1.5

--- week3/code/head.py
+++ week3/code/head.py
@@ -6,50 +6,39 @@ def Brown_motion(T,m):
 	diff_T=np.diff(T)
 	diff_W=np.random.randn(m,len(diff_T))
 	diff_W=diff_W*np.sqrt(diff_T)
+	#布朗运动有m个分量，T是时间剖分
 	W=np.zeros((m,len(T)))
 	W[:,1:]=np.cumsum(diff_W,1)
 	return W
 
 def Euler(T,W,a,b,X_0):
-	Y=np.zeros(np.shape(W))
+	Y=np.zeros((len(X_0),len(T)))
 	Y[0]=X_0
 	diff_T=np.diff(T)
 	diff_W=np.diff(W)
 	for i in range(len(diff_T)):
-		Y[:,i+1]=Y[:,i]+a*Y[:,i]*diff_T[i]+b*diff_W[:,i]*Y[:,i]
+		Y[:,i+1]=Y[:,i]+a(T[i],Y[:,i])*diff_T[i]+np.dot(b(T[i],Y[:,i]),diff_W[:,i])
 	return Y
-######################
-N=5
-m=1
-lam=2
-mu=1
-X_0=1
-T=np.linspace(0,1,pow(10,N)+1)
-5
-n=1
-W=Brown_motion(T,m)
-X_true=X_0*np.exp((lam-0.5*mu*mu)*T+mu*W)
-plt.plot(T,X_true.transpose())
-T1=np.linspace(0,1,pow(10,n)+1)
-W1=W[:,::pow(10,N-n)]
-X=Euler(T1,W1,lam,mu,X_0)
-plt.scatter(T1,X,color="red")
-plt.show()
 
-###############################
-'''
-s1=time.time()
-for n in range(N):
-	error=0
-	for j in range(1000):
-		W=Brown_motion(T,m)
-		X_true=X_0*np.exp((lam-0.5*mu*mu)*T+mu*W)
-		T1=np.linspace(0,1,pow(10,n)+1)
-		W1=W[:,::pow(10,N-n)]
-		X=Euler(T1,W1,lam,mu,X_0)
-		error+=abs(X_true[:,-1]-X[:,-1])
-	print(error/1000)
-s2=time.time()		
-print(s2-s1)		
-'''	
+def Milstein(T,W,a,b,db,X_0):
+	Y=np.zeros((len(X_0),len(T)))
+	Y[0]=X_0
+	diff_T=np.diff(T)
+	diff_W=np.diff(W)
+	for i in range(len(diff_T)):
+		Y[:,i+1]=Y[:,i]+a(T[i],Y[:,i])*diff_T[i]+np.dot(b(T[i],Y[:,i]),diff_W[:,i])+0.5*np.dot(np.dot(b(T[i],Y[:,i]),db(T[i],Y[:,i])),diff_W[:,i]**2-diff_T[i])
+	return Y
+def order_(T,W,a,b,da,db,dda,ddb,at,bt,X_0):
+	Y=np.zeros((len(X_0),len(T)))
+	Y[0]=X_0
+	diff_T=np.diff(T)
+	diff_W=np.diff(W)
+	diff_Z=(1/np.sqrt(3)*np.random.randn(np.shape(diff_W)[0],np.shape(diff_W)[1])+diff_W/np.sqrt(diff_T))*0.5*diff_T**1.5
+	for i in range(len(diff_T)):
+		Y[:,i+1]=Y[:,i]+a(T[i],Y[:,i])*diff_T[i]+np.dot(b(T[i],Y[:,i]),diff_W[:,i])+0.5*np.dot(np.dot(b(T[i],Y[:,i]),db(T[i],Y[:,i])),diff_W[:,i]**2-diff_T[i])
+		Y[:,i+1]=Y[:,i+1]+diff_T[i]**2/2*(at(T[i],Y[:,i])+a(T[i],Y[:,i])*da(T[i],Y[:,i])+0.5*b(T[i],Y[:,i])**2*dda(T[i],Y[:,i]))+b(T[i],Y[:,i])*da(T[i],Y[:,i])*diff_Z[:,i]
+		Y[:,i+1]=Y[:,i+1]+(bt(T[i],Y[:,i])+a(T[i],Y[:,i])*db(T[i],Y[:,i])+0.5*b(T[i],Y[:,i])**2*ddb(T[i],Y[:,i]))*(diff_T[i]*diff_W[:,i]-diff_Z[:,i])
+		Y[:,i+1]=Y[:,i+1]+0.5*(b(T[i],Y[:,i])*db(T[i],Y[:,i])**2+b(T[i],Y[:,i])**2*ddb(T[i],Y[:,i]))*(1/3*diff_W[:,i]**3-diff_W[:,i]*diff_T[i])
+	return Y
+		
 
---
+++ week3/code/order_1.5.py
@@ -0,0 +1,35 @@
+import head
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+N=5
+m=1
+lam=np.array([2])
+mu=np.array([1])
+X_0=np.array([1])
+T=np.linspace(0,1,pow(10,N)+1)
+n=1
+W=head.Brown_motion(T,m)
+X_true=X_0*np.exp((lam-0.5*np.inner(mu,mu))*T+np.dot(mu,W))
+plt.plot(T,X_true.transpose())
+T1=np.linspace(0,1,pow(10,n)+1)
+W1=W[:,::pow(10,N-n)]
+X=head.order_(T1,W1,lambda t,y:lam*y,lambda t,y:mu*y,lambda t,y:lam,lambda t,y:mu,lambda t,y:0,lambda t,y:0,lambda t,y:0,lambda t,y:0,X_0)
+plt.scatter(T1,X,color="red")
+plt.show()
+######################################
+'''
+s1=time.time()
+for n in range(N):
+	error=0
+	for j in range(1000):
+		W=head.Brown_motion(T,m)
+		X_true=X_0*np.exp((lam-0.5*mu*mu)*T+mu*W)
+		T1=np.linspace(0,1,pow(10,n)+1)
+		W1=W[:,::pow(10,N-n)]
+		X=head.order_(T1,W1,lambda t,y:lam*y,lambda t,y:mu*y,lambda t,y:lam,lambda t,y:mu,lambda t,y:0,lambda t,y:0,lambda t,y:0,lambda t,y:0,X_0)
+		error+=abs(X_true[:,-1]-X[:,-1])
+	print(error/1000)
+s2=time.time()		
+print(s2-s1)		
+'''
